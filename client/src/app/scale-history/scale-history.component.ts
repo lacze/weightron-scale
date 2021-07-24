95f05b5834eaa634ae84925eead89af7bfdf34eb
Lasse Linden <lasse.a.linden@gmail.com>
2021-07-24 12:42:57 +0300

    #3 added base ui for scale history - added placeholder data

--- client/src/app/scale-history/scale-history.component.ts
+++ client/src/app/scale-history/scale-history.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scale-history.component.scss']
 })
 export class ScaleHistoryComponent implements OnInit {
+  private placeholderHistoryListData = [
+    { timestamp: '2021-07-24 (placeholder)', weigh: 10 },
+    { timestamp: '2021-07-24 (placeholder)', weigh: 20 },
+    { timestamp: '2021-07-24 (placeholder)', weigh: 30 }]
+  
+    public historyList: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    // TODO: Remove when removing placeholder data
+    this.historyList = this.placeholderHistoryListData;
   }
 
 }
